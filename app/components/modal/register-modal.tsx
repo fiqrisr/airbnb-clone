5d3dff9788cedf74304fe02f519406c26c86d9a6
Fiqri Syah Redha <wibulo27@gmail.com>
2023-04-08 23:58:11 +0700

    finish register modal ui

--- app/components/modal/register-modal.tsx
+++ app/components/modal/register-modal.tsx
@@ -5,8 +5,9 @@ import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 
-import { Heading, Input, Modal } from "@/app/components";
+import { Button, Heading, Input, Modal } from "@/app/components";
 import { useRegisterModal } from "@/app/hooks";
 
 const RegisterModal = () => {
@@ -63,9 +64,38 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => registerModal.onClose())
-      .catch((error) => console.error(error))
+      .catch((error) => toast.error("Something went wrong"))
       .finally(() => setIsLoading(false));
   };
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with GitHub"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center justify-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            className="text-neutral-800 cursor-pointer hover:underline"
+            onClick={registerModal.onClose}
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <Modal
@@ -75,6 +105,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     ></Modal>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 
 import { ClientOnly, RegisterModal, Navbar } from "@/app/components";
+import { ToasterProvider } from "@/app/providers";
 
 export const metadata = {
   title: "Airbnb",
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
